4d48eb1dd3ac145713380509a56e779699f3a567
hankliu <397694072@qq.com>
2024-04-16 16:18:24 +0800

    feat(site): add base and public path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 import path from 'path';
 
-export default defineConfig({
+const config: ReturnType<typeof defineConfig> = {
   alias: {
     '@hankliu/rc-footer': path.resolve('src'),
     '@hankliu/rc-footer/es': path.resolve('src'),
@@ -11,4 +11,23 @@ export default defineConfig({
     name: 'Image',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
-});
+}
+
+// 是否通过github actions部署
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if (isGithubActions) {
+  // @ts-ignore
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
+  // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
+  config.base  = `/${repo}/`;
+  // 用于为应用设置基础路径(Link组件中，类似 history 里面的basename，在路由跳转时自动加前缀)
+  // 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
+  config.publicPath = `/${repo}/`;
+
+  console.log("next config is:", config);
+}
+
+export default defineConfig(config);
+
